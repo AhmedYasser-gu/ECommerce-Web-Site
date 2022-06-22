d93a57924912cee3451230fc427ea5042ab37fc2
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-06-22 11:06:37 +0200

    Update styles.js

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -3,11 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   content: {
-    flexGrow: 1,
+    flexGrow: 2,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+    padding: theme.spacing(2),
   },
   root: {
-    flexGrow: 1,
+    flexGrow: 2,
   },
 }));
